ea4fcb1ead729303478327ea9b67ed8a34217b88
erdnando <erdnando@gmail.com>
2021-11-18 16:48:16 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const parseArgs = require('minimist');
+
+// App de Express
+const app = express();
+
+// Node Server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+const args = parseArgs(process.argv.slice(2));
+const { name = 'default', port = process.env.PORT } = args;
+
+
+
+// Path público
+//const publicPath = path.resolve( __dirname, 'public' );
+//app.use( express.static( publicPath ) );
+
+app.get('/', (req, resp) => {
+    resp.send(`socket.io...on : ${port}`);
+});
+
+
+
+//server.listen(process.env.PORT, (err) => {
+server.listen(+port, '0.0.0.0', (err) => {
+
+    if (err) throw new Error(err);
+
+    //console.log('Servidor corriendo en puerto', process.env.PORT);
+    console.log(`Node [${name}] listens on http://127.0.0.1:${port}.`);
+});
---
+++ sockets/socket.js
@@ -0,0 +1,100 @@
+const { io } = require('../index');
+
+var contador = 0;
+// Mensajes de Sockets
+io.on('connection', socket => {
+    console.log('Nuevo socket conectado');
+
+
+    socket.on('login', (payload) => {
+        //1 publish into backend topics aka kaftka or rabbitmq
+
+        //2 subscribe for backend topic response
+
+        //3 on response emit event to frontend
+        var autenticado = false;
+        var mensaje = "";
+
+        if (payload.email == "xxx") {
+            autenticado = true;
+            mensaje = "Usuario valido";
+        } else {
+            autenticado = false;
+            mensaje = "Usuario no valido";
+        }
+
+        //3.1.1 envia a todos
+        //io.emit('ONINCREMENTAR', { contador: contador });
+        //3.1.2 o envia solo al cte conectado
+        socket.emit('ONLOGIN', { autenticado: autenticado, email: payload.email, msg: mensaje });
+
+    });
+
+    socket.on('incrementar', (payload) => {
+        //1 publish into backend topics aka kaftka or rabbitmq
+
+        //2 subscribe for backend topic response
+
+        //3 on response emit event to frontend
+
+        contador++;
+        console.log('incrementar', contador);
+        //3.1.1 envia a todos
+        //io.emit('ONINCREMENTAR', { contador: contador });
+        //3.1.2 o envia solo al cte conectado
+        socket.emit('ONINCREMENTAR', { contador: contador });
+
+    });
+
+    socket.on('decrementar', (payload) => {
+        //1 publish into backend topics aka kaftka or rabbitmq
+
+        //2 subscribe for backend topic response
+
+        //3 on response emit event to frontend
+
+        contador--;
+        console.log('decrementar', contador);
+        //3.1.1 envia a todos
+        //io.emit('ONDECREMENTAR', { contador: contador });
+        //3.1.2 o envia solo al cte conectado
+        socket.emit('ONDECREMENTAR', { contador: contador });
+
+    });
+
+    socket.on('getContador', (payload) => {
+        //1 publish into backend topics aka kaftka or rabbitmq
+
+        //2 subscribe for backend topic response
+
+        //3 on response emit event to frontend
+
+        console.log('getContador', contador);
+        //3.1.1 envia a todos
+        //io.emit('ONGETCONTADOR', { contador: contador });
+        //3.1.2 o envia solo al cte conectado
+        io.emit('ONGETCONTADOR', { contador: contador });
+
+    });
+
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    /*
+    Then, after a client requests to join a room, 
+    the WebSocket will capture and broadcast events from one client to all other clients in the same room.
+
+        // Register "join" events, requested by a connected client
+        socket.on("join", function(room) {
+            // join channel provided by client
+            socket.join(room)
+                // Register "image" events, sent by the client
+            socket.on("image", function(msg) {
+                // Broadcast the "image" event to all other clients in the room
+                socket.broadcast.to(room).emit("image", msg);
+            });
+        })*/
+
+});
